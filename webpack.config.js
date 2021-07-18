c0020b178714fc06a8b5c3c4d84076ed8ed841f9
Ralph-tech <58685004+Ralph-tech@users.noreply.github.com>
2021-07-18 21:45:32 +0300

    Add

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.join(__dirname, 'dist'),
   },
   devServer: {
     contentBase: './deploy',
@@ -21,6 +20,10 @@ module.exports = {
           "css-loader",
           "sass-loader"
         ]
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.jpe?g$/i,
