81a08afcef43c4c32e6b3063cc32ca8c0e863023
Ralph-tech <58685004+Ralph-tech@users.noreply.github.com>
2021-07-22 16:45:33 +0300

    Add assets

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ class Game extends Phaser.Game {
   constructor () {
     super(config);
     this.scene.add('Game', GameScene);
-    this.scene.add('Preloader', PreloaderScene)
+    this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Title', TitleScene);
+    this.scene.add('GamePlay', GamePlayScene);
+    this.scene.add('LeaderBoard', LeaderBoardScene);
     this.scene.start('Game');
   }
 }
