3421acf7e74a67dac8998ce70347fe4736a0150e
Ralph-tech <58685004+Ralph-tech@users.noreply.github.com>
2021-07-25 13:22:14 +0300

    Add more tests

--- test/getName.test.js
+++ test/getName.test.js
@@ -5,6 +5,11 @@ it('returns the value found in the form input element', () => {
   expect(getName()).toBe('John');
 });
 
+it('Checks for the form proper input value', () => {
+  document.body.innerHTML = '<input value="Ralph">';
+  expect(getName()).not.toBe('John');
+});
+
 it('returns unknown if the value found in the form input element is an empty string', () => {
   document.body.innerHTML = '<input>';
   expect(getName()).toBe('unknown');
--- test/sortScores.test.js
+++ test/sortScores.test.js
@@ -4,7 +4,7 @@ import sortScores from '../src/js/actions/sortScores';
 
 const dataExample = [
   {
-    user: 'Aymen',
+    user: 'Ralph',
     score: 10,
   },
   {
@@ -18,18 +18,28 @@ it('sorts the array by alphabetical order if the scores are equal', () => {
   expect(sorted[0].user).toEqual('Max');
 });
 
+it('test for unsorted array', () => {
+  const sorted = sortScores(dataExample);
+  expect(sorted[0].user).not.toEqual('Ralph');
+});
+
 const sameScore = [
   {
     user: 'Max',
     score: 60,
   },
   {
-    user: 'Aymen',
+    user: 'Ralph',
     score: 60,
   },
 ];
 
 it('sorts the array by alphabetical order if the scores are equal', () => {
   const sorted = sortScores(sameScore);
-  expect(sorted[0].user).toEqual('Aymen');
+  expect(sorted[0].user).toEqual('Max');
+});
+
+it('check if the array has been sorted', () => {
+  const sorted = sortScores(sameScore);
+  expect(sorted[0].user).not.toEqual('Ralph');
 });
